24b6fdf839076d736d0757f60d2a7b4ccd98a693
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-09-19 08:00:36 +0300

    simple_interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,8 @@
+# Assign values 
+principal = 1000 # Amount
+rate = 0.05 # annual interest rate
+time = 3 # representing 3 years
+# Calculate simple interest
+interest = principal * rate * time
+# reslut 
+print(f"calculate simple is: {interest}")
