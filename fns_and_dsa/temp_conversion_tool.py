8a33f52ce2d54909d7d09db36e424766d1ca53b2
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-10-03 10:58:59 +0300

    temp_con

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,18 +1,14 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
 CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
 FAHRENHEIT_OFFSET = 32
-
 def convert_to_celsius(fahrenheit):
     global FAHRENHEIT_TO_CELSIUS_FACTOR
     celsius = (fahrenheit - FAHRENHEIT_OFFSET) * FAHRENHEIT_TO_CELSIUS_FACTOR
     return celsius
-
 def convert_to_fahrenheit(celsius):
     global CELSIUS_TO_FAHRENHEIT_FACTOR
     fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + FAHRENHEIT_OFFSET
     return fahrenheit
-# main function :
-
 def main():
     try:
         
