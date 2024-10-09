c4361821f3b800a940620da85ff158a94ff148ce
ahmedabdelhady <ahmedabdelhadye@gmail.com>
2024-10-09 22:24:42 +0300

    task edit

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -1,7 +1,7 @@
 def safe_divide(numerator, denominator):
     try:
-        num = float(numerator)
-        denom = float(denominator)
+        num = int(numerator)
+        denom = int(denominator)
 
         if denom == 0:
             return "Error: Cannot divide by zero."
